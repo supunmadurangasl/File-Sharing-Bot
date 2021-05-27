fa1476ae1766b522fa2d595b9302bd13b7bedfa3
supun maduranga <83456156+supunmadurangasl@users.noreply.github.com>
2021-05-27 13:48:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -23,7 +23,10 @@ FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 #start message
-START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\nI can store private files in Specified Channel and other users can access it from special link.")
+START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\n,I am Telegram File to Link Generator Bot.
+📂 Send me a file to get link.
+
+POWERD BY 👨‍💻 ©@slbotzone 🇱🇰.")
 try:
     ADMINS=[]
     for x in (os.environ.get("ADMINS", "").split()):
